c561296df6bf94ea4aa805c202d819e8749e49d8
Manuel Kaufmann <humitos@gmail.com>
2018-04-30 20:19:38 -0500

    Remove now unused signal

--- readthedocs/projects/signals.py
+++ readthedocs/projects/signals.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import
 import django.dispatch
 from django.dispatch import receiver
 
-from readthedocs.core.utils import trigger_build
 from readthedocs.oauth.utils import attach_webhook
 
 
@@ -24,10 +23,3 @@ def handle_project_import(sender, **kwargs):
     request = kwargs.get('request')
 
     attach_webhook(project=project, request=request)
-
-
-# TODO: move this to ImportWizardView.trigger_initial_build
-# @receiver(project_import)
-# def trigger_initial_build(sender, request, **kwargs):
-#     project = sender
-#     trigger_build(project)
